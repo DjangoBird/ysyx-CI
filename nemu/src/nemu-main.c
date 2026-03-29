d3dcf749097e56ddde44009a04d2f4157c01be7e
tracer-ysyx <tracer@ysyx.org>
2026-03-29 23:59:48 +0800

    > compile NEMU ysyx_22040000 jiangyuhan Linux Ubuntu22 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  23:59:48 up  3:25,  1 user,  load average: 0.73, 0.34, 0.17

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -14,6 +14,10 @@
 ***************************************************************************************/
 
 #include <common.h>
+#include <stdio.h>
+
+word_t expr(char *e, bool *success);
+void init_regex();
 
 void init_monitor(int, char *[]);
 void am_init_monitor();
@@ -21,6 +25,46 @@ void engine_start();
 int is_exit_status_bad();
 
 int main(int argc, char *argv[]) {
+
+#ifndef CONFIG_TARGET_AM
+  // 表达式批量测试模式: ./build/riscv32-nemu-interpreter expr-test
+  if (argc == 2 && strcmp(argv[1], "expr-test") == 0) {
+    init_regex();
+
+    FILE *fp = fopen("input", "r");
+    assert(fp != NULL);
+
+    char line[65536 + 32];
+    char expr_str[65536];
+    unsigned std_result;
+    int cnt = 0;
+
+    while (fgets(line, sizeof(line), fp) != NULL) {
+      if (sscanf(line, "%u %[\n\t\v\f\r ]%[^\n]", &std_result, expr_str, expr_str) < 2) {
+        // 简化: 再用更通用的方式
+        if (sscanf(line, "%u %[^\n]", &std_result, expr_str) != 2) {
+          continue;
+        }
+      }
+
+      bool success = true;
+      word_t nemu_result = expr(expr_str, &success);
+
+      if (!success || nemu_result != std_result) {
+        printf("mismatch: std=%u nemu=%u expr=%s\n",
+               std_result, nemu_result, expr_str);
+        assert(0);
+      }
+
+      cnt++;
+    }
+
+    printf("expr-test passed, %d cases.\n", cnt);
+    fclose(fp);
+    return 0;
+  }
+#endif
+
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
