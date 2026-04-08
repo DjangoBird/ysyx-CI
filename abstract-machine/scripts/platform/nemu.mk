cfc28e8748e48cc25ac4859149cf6e697a789428
jiangyuhan <jiangyuhan233@mails.ucas.ac.cn>
2026-04-08 23:52:01 +0800

    Feat: add string-function

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -22,20 +22,20 @@ insert-arg: image
 	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) $(MAINARGS_PLACEHOLDER) "$(mainargs)"
 
 image: image-dep
-    @$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
-    @echo + OBJCOPY "->" $(IMAGE_REL).bin
-    @$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
+	@echo + OBJCOPY "->" $(IMAGE_REL).bin
+	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 # 交互式：保留 sdb，需要手动输入 c 运行
 run: insert-arg
-    $(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS_BASE)" IMG=$(IMAGE).bin
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS_BASE)" IMG=$(IMAGE).bin
 
 # 批处理：直接运行客户程序，不进入交互式 sdb
 run-batch: insert-arg
-    $(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS_BASE) -b" IMG=$(IMAGE).bin
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS_BASE) -b" IMG=$(IMAGE).bin
 
 # gdb 下调试时通常希望交互式，这里不加 -b
 gdb: insert-arg
-    $(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS_BASE)" IMG=$(IMAGE).bin
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS_BASE)" IMG=$(IMAGE).bin
 
 .PHONY: insert-arg
