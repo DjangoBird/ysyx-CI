2c16d95478fa26ea690c16f392bfcdcdcd8cd1d3
jiangyuhan <jiangyuhan233@mails.ucas.ac.cn>
2026-05-15 17:53:14 +0800

    ADD:isa-difftest-checkregs

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,19 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  if (cpu.pc != ref_r->pc) {
+    printf("pc mismatch: dut=" FMT_WORD ", ref=" FMT_WORD "\n", cpu.pc, ref_r->pc);
+  }
+
+  for (int i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); i++) {
+    if (cpu.gpr[i] != ref_r->gpr[i]) {
+      printf("gpr[%d] mismatch at pc=" FMT_WORD ": dut=" FMT_WORD ", ref=" FMT_WORD "\n",
+             i, pc, cpu.gpr[i], ref_r->gpr[i]);
+      return false;
+    }
+  }
+
+  return true;
 }
 
 void isa_difftest_attach() {
