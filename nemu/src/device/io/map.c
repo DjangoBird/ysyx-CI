94c94c7b322d7bbcfeef822cf48a4cf9765ecadf
tracer-ysyx <tracer@ysyx.org>
2026-05-17 12:06:06 +0800

    > compile NEMU ysyx_22040000 jiangyuhan Linux Ubuntu22 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  12:06:06 up  2:25,  1 user,  load average: 0.03, 0.07, 0.04

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -58,6 +58,10 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+  IFDEF(CONFIG_DTRACE, if (DTRACE_COND) {
+    log_write("dtrace R %s off=" FMT_PADDR " len=%d data=" FMT_WORD "\n",
+        map->name, offset, len, ret);
+  });
   return ret;
 }
 
@@ -67,4 +71,8 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
+  IFDEF(CONFIG_DTRACE, if (DTRACE_COND) {
+    log_write("dtrace W %s off=" FMT_PADDR " len=%d data=" FMT_WORD "\n",
+        map->name, offset, len, data);
+  });
 }
