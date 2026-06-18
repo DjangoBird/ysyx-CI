d420be360652c0d005d5188aab5c91becb1c2d52
tracer-ysyx <tracer@ysyx.org>
2026-06-18 20:19:20 +0800

    > compile NEMU ysyx_22040000 jiangyuhan Linux django-Legion-Y9000P-IRX8 6.17.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 19:30:42 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:19:20 up  2:22,  1 user,  load average: 1.80, 1.41, 1.04

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -39,7 +39,15 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+  uintptr_t sp = (uintptr_t)kstack.end;
+  sp = sp / sizeof(uintptr_t) * sizeof(uintptr_t);
+
+  Context *c = (Context *)(sp - sizeof(Context));
+  *c = (Context) { 0 };
+  c->mepc = (uintptr_t)entry;
+  c->mstatus = 0x1800;
+  c->gpr[10] = (uintptr_t)arg;
+  return c;
 }
 
 void yield() {
